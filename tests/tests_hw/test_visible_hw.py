1bf8ae1016420c9869e9e2bba24ce083951242ab
ShiinaMashiro1 <SadMan.kk@yandex.ru>
2025-08-23 19:43:38 +0300

    8_hw_corrected

--- tests/tests_hw/test_visible_hw.py
+++ tests/tests_hw/test_visible_hw.py
@@ -6,35 +6,35 @@ from pages.accordion import Accordion
 from conftest import browser
 
 
-@pytest.mark.usefixtures("browser")  # если у вас фикстура называется exactly 'browser'
-class TestAccordionVisibility:
+@pytest.mark.usefixtures("browser")
+def test_visible_accordion(browser):
+    page = Accordion(browser)
+    page.open()
 
-    def test_visible_accordion(self, browser):
-        page = Accordion(browser)
-        page.open()
+    assert page.section1_content_p.is_displayed(), (
+        "Ожидали, что параграф в секции 1 будет видимым по умолчанию."
+    )
 
-        assert page.section1_content_p.is_displayed(), (
-            "Ожидали, что параграф в секции 1 будет видимым по умолчанию."
-        )
+    page.section1_heading.click()
 
-        page.section1_heading.click()
+    time.sleep(2)
 
-        time.sleep(2)
+    assert not page.section1_content_p.is_displayed(), (
+        "Ожидали, что параграф в секции 1 станет скрытым после клика по заголовку."
+    )
 
-        assert not page.section1_content_p.is_displayed(), (
-            "Ожидали, что параграф в секции 1 станет скрытым после клика по заголовку."
-        )
 
-    def test_visible_accordion_default(self, browser):
-        page = Accordion(browser)
-        page.open()
+@pytest.mark.usefixtures("browser")
+def test_visible_accordion_default(browser):
+    page = Accordion(browser)
+    page.open()
 
-        assert not page.section2_content_p1.is_displayed(), (
-            "Ожидали, что section2 п.1 скрыт по умолчанию."
-        )
-        assert not page.section2_content_p2.is_displayed(), (
-            "Ожидали, что section2 п.2 скрыт по умолчанию."
-        )
-        assert not page.section3_content_p.is_displayed(), (
-            "Ожидали, что section3 параграф скрыт по умолчанию."
-        )
+    assert not page.section2_content_p1.is_displayed(), (
+        "Ожидали, что section2 п.1 скрыт по умолчанию."
+    )
+    assert not page.section2_content_p2.is_displayed(), (
+        "Ожидали, что section2 п.2 скрыт по умолчанию."
+    )
+    assert not page.section3_content_p.is_displayed(), (
+        "Ожидали, что section3 параграф скрыт по умолчанию."
+    )
